b7ca8f84e45f6805b72c7ae06b427d7b33e916fa
Lucio Martinez <martinezlucio.com@gmail.com>
2015-08-23 00:43:54 -0300

    #5 - Enable create method to be used on API and skip CSRF exception

--- app/controllers/scores_controller.rb
+++ app/controllers/scores_controller.rb
@@ -1,20 +1,26 @@
 class ScoresController < InheritedResources::Base
-  before_action :authenticate_player!
+  before_action :authenticate_player!, except: [ :create ]
 
   def index
     @scores = Score.all
   end
 
-  #def create 
-    #@player = Player.find(params[:player_id])
+  def create 
+    @player = Player.find(params[:player_id])
 
-    #@score = @player.scores.create(score_params)
-  #end
+    @score = @player.scores.create(score_params)
+
+    if @score.present? and @score.id.present?
+      render json: @score, status: :ok
+    else
+      render json: @score, status: :unprocessable_entity
+    end
+  end
 
   private
 
     def score_params
-      params.require(:score).permit(:score, :player)
+      params.require(:score).permit(:score)
     end
 end
 
