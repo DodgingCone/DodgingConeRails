f7a0ca0c39412a1a2774c530d6a76228243a3705
Lucio Martinez <martinezlucio.com@gmail.com>
2015-11-04 01:01:55 -0300

    #20 Enable validation for score so a same score by the same player cannot be uploaded

--- app/controllers/api/scores_controller.rb
+++ app/controllers/api/scores_controller.rb
@@ -10,7 +10,15 @@ class Api::ScoresController < Api::ApiController
   def create
     @player = current_player
 
-    @score = @player.scores.create(score_params)
+    scores = @player.scores
+
+    # Forbid same score to be saved
+    if scores.find_by_score(params["score"]) != nil
+      render json: nil, status: :bad_request
+      return
+    end
+
+    @score = scores.create(score_params)
 
     if @score.present? and @score.id.present?
       render json: @score, status: :created
